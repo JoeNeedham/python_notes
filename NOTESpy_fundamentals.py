81157eb7d55027c296cfd5241b233a6d8f60f7d2
joeneedum <joeneedum@gmail.com>
2021-07-27 22:38:29 -0400

    last

--- NOTESpy_fundamentals.py
+++ NOTESpy_fundamentals.py
@@ -184,6 +184,50 @@ beCheerful(repeat=3, name="kb")		# output: good morning kb (repeated on 3 lines)
 arr = [1,3,5,7]
 arr[0], arr[1] = arr[1], arr[0]
 
+# traditional
+if stacks >= 3:
+    print('Coding Dojo')
+else:
+    print('You are not Coding Dojo!')
+# ternary
+print('Coding Dojo' if stacks >= 3 else 'You are not Coding Dojo!')
+
+# anonymous functions 
+
+lambda num1, num2: num1+num2
+
+# create a new list, with a lambda as an element
+my_list = ['test_string', 99, lambda x : x ** 2]
+# access the value in the list
+print(my_list[2]) # will print a lambda object stored in memory
+# invoke the lambda function, passing in 5 as the argument
+my_list[2](5)
+
+# define a function that takes one input that is a function
+def invoker(callback):
+    # invoke the input pass the argument 2
+    print(callback(2))
+invoker(lambda x: 2 * x)
+invoker(lambda y: 5 + y)
+
+add10 = lambda x: x + 10 # store lambda expression in a variable
+add10(2)  # returns 12
+add10(98) # returns 108
+
+def incrementor(num):
+    start = num
+    return lambda x: num + x
+
+# create a list
+my_arr = [1,2,3,4,5]
+# define a function that squares values
+def square(num):
+    return num ** 2
+# invoke map function
+print(list(map(square, my_arr)))
+
+
+
 
 
 
